c84e80d07fce1bd7eea09a8da7388f070df783af
Tony380 <tonygon2@yahoo.es>
2021-03-30 19:34:49 +0200

    Profile, image and update

--- users/views.py
+++ users/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
-from .forms import RegisterForm
+from .forms import RegisterForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib import messages
 from django.contrib.auth import login, logout, authenticate, get_user
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.messages.views import SuccessMessageMixin
+from .models import Profile
 
 
 def register(request):
@@ -15,6 +16,7 @@ def register(request):
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(request, username=username, password=password)
+            Profile.objects.create(user=user)
             login(request, user)
             messages.success(request, 'Your account has been created successfully!')
             return redirect('home:index')
@@ -26,7 +28,26 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    """ User's profile page with data changes possibilty"""
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,
+                                   instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Your account has been updated successfully!')
+            return redirect('users:profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
 
 
 class Login(SuccessMessageMixin, LoginView):
