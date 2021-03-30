d62783cea4d84f683ec8d8e634767a9dcca7318b
Tony380 <tonygon2@yahoo.es>
2021-03-30 14:29:32 +0200

    end register login logout

--- users/views.py
+++ users/views.py
@@ -8,7 +8,6 @@ from django.contrib.messages.views import SuccessMessageMixin
 
 
 def register(request):
-
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -17,29 +16,25 @@ def register(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(request, username=username, password=password)
             login(request, user)
-            messages.success(request, f'Hi {username}! Your account has been created successfully!')
+            messages.success(request, 'Your account has been created successfully!')
             return redirect('home:index')
-
     else:
         form = RegisterForm()
-
+        
     return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
 def profile(request):
-
     return render(request, 'users/profile.html')
 
 
 class Login(SuccessMessageMixin, LoginView):
-
     success_message = "You have been logged in successfully!"
 
 
 @login_required
 def logout_view(request):
-
     user = get_user(request)
     logout(request)
     messages.success(request, f"Good bye {user.username}! You have been logged out successfully!")
