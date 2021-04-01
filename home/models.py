d7da14a41f86346f033e26bb55465e5fbf48ce12
Tony380 <tonygon2@yahoo.es>
2021-04-01 11:42:51 +0200

    small changes

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
 
     title = models.CharField(max_length=100)
     content = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(auto_now=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
 
     def __str__(self):
