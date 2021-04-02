e4b15b589d2ff616d521e53daaa2ea194bdb168d
Tony380 <tonygon2@yahoo.es>
2021-04-02 19:08:52 +0200

    Travis CI

--- blog/settings.py
+++ blog/settings.py
@@ -149,4 +149,4 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_S3_REGION_NAME = 'eu-west-3'
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), test_runner=False)
